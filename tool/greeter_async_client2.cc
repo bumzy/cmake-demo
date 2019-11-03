9a2642899d78151f77b83fb3add9138ef7aa7597
chenming <bumzycm@gmail.com>
2019-11-03 18:13:51 +0800

    fix code style

--- tool/greeter_async_client2.cc
+++ tool/greeter_async_client2.cc
@@ -35,23 +35,25 @@ using grpc::ClientAsyncResponseReader;
 using grpc::ClientContext;
 using grpc::CompletionQueue;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
 using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
 
-class GreeterClient {
-  public:
+class GreeterClient
+{
+public:
     explicit GreeterClient(std::shared_ptr<Channel> channel)
-            : stub_(Greeter::NewStub(channel)) {}
+        : stub_(Greeter::NewStub(channel)) {}
 
     // Assembles the client's payload and sends it to the server.
-    void SayHello(const std::string& user) {
+    void SayHello(const std::string &user)
+    {
         // Data we are sending to the server.
         HelloRequest request;
         request.set_name(user);
 
         // Call object to store rpc data
-        AsyncClientCall* call = new AsyncClientCall;
+        AsyncClientCall *call = new AsyncClientCall;
 
         // stub_->PrepareAsyncSayHello() creates an RPC object, returning
         // an instance to store in "call" but does not actually start the RPC
@@ -66,20 +68,21 @@ class GreeterClient {
         // Request that, upon completion of the RPC, "reply" be updated with the
         // server's response; "status" with the indication of whether the operation
         // was successful. Tag the request with the memory address of the call object.
-        call->response_reader->Finish(&call->reply, &call->status, (void*)call);
-
+        call->response_reader->Finish(&call->reply, &call->status, (void *)call);
     }
 
     // Loop while listening for completed responses.
     // Prints out the response from the server.
-    void AsyncCompleteRpc() {
-        void* got_tag;
+    void AsyncCompleteRpc()
+    {
+        void *got_tag;
         bool ok = false;
 
         // Block until the next result is available in the completion queue "cq".
-        while (cq_.Next(&got_tag, &ok)) {
+        while (cq_.Next(&got_tag, &ok))
+        {
             // The tag in this example is the memory location of the call object
-            AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
+            AsyncClientCall *call = static_cast<AsyncClientCall *>(got_tag);
 
             // Verify that the request was completed successfully. Note that "ok"
             // corresponds solely to the request for updates introduced by Finish().
@@ -95,10 +98,10 @@ class GreeterClient {
         }
     }
 
-  private:
-
+private:
     // struct for keeping state and data information
-    struct AsyncClientCall {
+    struct AsyncClientCall
+    {
         // Container for the data we expect from the server.
         HelloReply reply;
 
@@ -108,7 +111,6 @@ class GreeterClient {
 
         // Storage for the status of the RPC upon completion.
         Status status;
-
 
         std::unique_ptr<ClientAsyncResponseReader<HelloReply>> response_reader;
     };
@@ -122,26 +124,28 @@ class GreeterClient {
     CompletionQueue cq_;
 };
 
-int main(int argc, char** argv) {
-
+int main(int argc, char **argv)
+{
 
     // Instantiate the client. It requires a channel, out of which the actual RPCs
     // are created. This channel models a connection to an endpoint (in this case,
     // localhost at port 50051). We indicate that the channel isn't authenticated
     // (use of InsecureChannelCredentials()).
     GreeterClient greeter(grpc::CreateChannel(
-            "localhost:50051", grpc::InsecureChannelCredentials()));
+        "localhost:50051", grpc::InsecureChannelCredentials()));
 
     // Spawn reader thread that loops indefinitely
     std::thread thread_ = std::thread(&GreeterClient::AsyncCompleteRpc, &greeter);
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++)
+    {
         std::string user("world " + std::to_string(i));
-        greeter.SayHello(user);  // The actual RPC call!
+        greeter.SayHello(user); // The actual RPC call!
     }
 
-    std::cout << "Press control-c to quit" << std::endl << std::endl;
-    thread_.join();  //blocks forever
+    std::cout << "Press control-c to quit" << std::endl
+              << std::endl;
+    thread_.join(); //blocks forever
 
     return 0;
 }
